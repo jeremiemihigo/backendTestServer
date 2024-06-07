48ba3050ad7fa37599b7a7938a2045308f84620d
unknown <grace1mihigo@gmail.com>
2024-06-07 12:09:46 +0200

    io

--- index.js
+++ index.js
@@ -417,7 +417,7 @@ app.get("/message", (req, res) => {
   return res.status(200).json("je suis un message");
 });
 const portIO = process.env.PORT || 5000;
-// io.listen(portIO);
+io.listen(portIO);
 //Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
