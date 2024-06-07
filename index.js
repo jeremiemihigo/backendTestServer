53ff94648915ac8c9bdf6092f8621f6e695bdb4c
unknown <grace1mihigo@gmail.com>
2024-06-07 23:41:03 +0200

    limit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb" }));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -415,7 +415,7 @@ io.on("connection", (socket) => {
 app.get("/message", (req, res) => {
   return res.status(200).json("je suis un message");
 });
-const portIO = process.env.PORT || 800;
+// const portIO = process.env.PORT || 800;
 // io.listen(portIO);
 //Start server
 app.listen(port, () => {
