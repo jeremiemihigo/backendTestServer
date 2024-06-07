077f8f41df563a9002ede5c2a03e29f387bfc434
unknown <grace1mihigo@gmail.com>
2024-06-07 16:35:48 +0200

    server

--- config/Connection.js
+++ config/Connection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(
+    "mongodb+srv://bboxx:XkpowCPp098pdicz@cluster0.oi8mi8c.mongodb.net/Tracker",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
   console.log("MongoDB connect");
 };
 
