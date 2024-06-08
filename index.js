0cedf3e2d7da281375a7f9f2b3527c1526e57139
unknown <grace1mihigo@gmail.com>
2024-06-08 12:52:11 +0200

    inde

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/Connection");
 const app = express();
-app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 require("dotenv").config();
 connectDB();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 10000;
 
 const postTracker = require("./Routes/Post");
 const getTracker = require("./Routes/Read");
@@ -415,8 +415,8 @@ io.on("connection", (socket) => {
 app.get("/", (req, res) => {
   return res.status(200).json("je suis un message de monsieur");
 });
-// const portIO = process.env.PORT || 800;
-// io.listen(portIO);
+const portIO = process.env.PORT || 800;
+io.listen(portIO);
 //Start server
 const hostname = "127.0.0.1";
 app.listen(port, hostname, () => {
